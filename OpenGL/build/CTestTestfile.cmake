e8d90a65319c392bdbe59bbcfbb6b883c541a6c8
Dominic-M1213 <109449189+Dominic-M1213@users.noreply.github.com>
2024-04-06 22:07:23 -0400

    Delete OpenGL/build directory

--- OpenGL/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/domma/OneDrive - The Pennsylvania State University/Documents/Classes/AERSP 424/OpenGL
-# Build directory: C:/Users/domma/OneDrive - The Pennsylvania State University/Documents/Classes/AERSP 424/OpenGL/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("_deps/glfw-build")
-subdirs("_deps/glew-build")
